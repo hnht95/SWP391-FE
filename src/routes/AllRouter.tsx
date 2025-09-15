82898ef1edd4d9823231e6615f6ff415dd84e2e4
hnht95 <haonnse171306@fpt.edu.vn>
2025-09-15 15:28:06 +0700

    update UI staff

--- src/routes/AllRouter.tsx
+++ src/routes/AllRouter.tsx
@@ -3,6 +3,9 @@ import LayoutUser from "../landing/user/LayoutUser";
 import LoginPage from "../auth/login/LoginPage";
 import HomePage from "../landing/user/component/HomePage";
 import SignUpPage from "../auth/signUp/SignUpPage";
+import ForgotPasswordPage from "../auth/forgotPassword/ForgotPasswordPage";
+import LayoutStaff from "../landing/staff/LayoutStaff";
+import HomePageStaff from "../landing/staff/component/HomePageStaff";
 
 const AllRouter = () => {
   return (
@@ -12,8 +15,16 @@ const AllRouter = () => {
         {/* <Route path="/aboutUs" element={<AboutUs />} /> */}
         {/* Thêm các route khác cần Header/Footer ở đây */}
       </Route>
+      <Route element={<LayoutStaff />}>
+        <Route path="/staff" element={<HomePageStaff />} />
+        <Route path="/staff/dashboard" element={<HomePageStaff />} />
+
+        {/* <Route path="/aboutUs" element={<AboutUs />} /> */}
+        {/* Thêm các route khác cần Header/Footer ở đây */}
+      </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
     </Routes>
   );
 };
